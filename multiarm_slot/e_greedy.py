ea052cc3ce30a716bc61162237eb878769152cf0
KazamiMichiru <matthewdm0816@gmail.com>
2022-09-14 21:19:26 +0800

    added multiple run

--- multiarm_slot/e_greedy.py
+++ multiarm_slot/e_greedy.py
@@ -1,7 +1,7 @@
 from multiarm_slot import *
 import random
 from typing import List
-
+import json
 
 if __name__ == "__main__":
 
@@ -11,24 +11,28 @@ if __name__ == "__main__":
     N_ITER = 1000
     EPSILON = 0.1
 
-    q = [0.] * N_STATES
-    n = [0] * N_STATES  
-    r_total = 0.
-    r_history = []
+    r_histories = []
+    for _ in range(10):
+        q = [0.] * N_STATES
+        n = [0] * N_STATES  
+        r_total = 0.
+        r_history = []
 
-    for _ in range(N_ITER):
-        # random choose
-        if random.random() <= EPSILON:
-            a = random.choice(range(N_STATES))
-        else:
-            a, _ = argmax(q)
+        for _ in range(N_ITER):
+            # random choose
+            if random.random() <= EPSILON:
+                a = random.choice(range(N_STATES))
+            else:
+                a, _ = argmax(q)
 
-        r = multiarm_slot.generate(a)
-        n[a] += 1
-        q[a] += (r - q[a]) / n[a]
-        r_total += r
-        r_history.append(r)
+            r = multiarm_slot.generate(a)
+            n[a] += 1
+            q[a] += (r - q[a]) / n[a]
+            r_total += r
+            r_history.append(r)
 
-    plot_reward_history(r_history, "e_greedy")
-    print(f"Estimated Q-values: {q}")
-    print(f"Average Reward: {r_total / sum(n)}")
+        print(f"Estimated Q-values: {q}")
+        print(f"Average Reward: {r_total / sum(n)}")
+        r_histories.append(r_history)
+    
+    json.dump(r_histories, open("e_greedy.json", "w"))
--- multiarm_slot/ucb.py
+++ multiarm_slot/ucb.py
@@ -2,6 +2,7 @@ from multiarm_slot import *
 import random
 from typing import List
 import numpy as np
+import json
 
 if __name__ == "__main__":
 
@@ -10,23 +11,28 @@ if __name__ == "__main__":
     N_ITER = 1000
     C = 0.5
 
-    q = np.zeros(N_STATES)
-    n = np.zeros(N_STATES)
-    r_total = 0.
-    r_history = []
+    r_histories = []
+    for _ in range(10):
+        q = np.zeros(N_STATES)
+        n = np.zeros(N_STATES)
+        r_total = 0.
+        r_history = []
 
-    for _ in range(N_ITER):
-        # random choose
-        upper_bound = q + C * np.sqrt((np.log(n.sum()) / n))
-        a, _ = argmax(upper_bound)
+        for _ in range(N_ITER):
+            # random choose
+            upper_bound = q + C * np.sqrt((np.log(n.sum()) / n))
+            a, _ = argmax(upper_bound)
 
-        r = multiarm_slot.generate(a)
-        n[a] += 1
-        q[a] += (r - q[a]) / n[a]
-        r_history.append(r)
-        r_total += r
+            r = multiarm_slot.generate(a)
+            n[a] += 1
+            q[a] += (r - q[a]) / n[a]
+            r_history.append(r)
+            r_total += r
 
-    plot_reward_history(r_history, "ucb")
-    print(f"Estimated Q-values: {q}")
-    print(f"Average Reward: {r_total / sum(n)}")
+        print(f"Estimated Q-values: {q}")
+        print(f"Average Reward: {r_total / sum(n)}")
+        r_histories.append(r_history)
+    
+    json.dump(r_histories, open("ucb.json", "w"))
+    
     
