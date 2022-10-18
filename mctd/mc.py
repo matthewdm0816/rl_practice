86ae4c7e659bc6229bd9bb70faaadcc468a41c50
KazamiMichiru <matthewdm0816@gmail.com>
2022-10-19 06:17:40 +0800

    add MC, sarsa

--- mctd/mc.py
+++ mctd/mc.py
@@ -1,33 +1,81 @@
+from http.client import EXPECTATION_FAILED
 from utils import *
 import copy
 from matplotlib import pyplot as plt
 import seaborn as sns
+from concurrent.futures import ProcessPoolExecutor, wait
 H, W = 4, 12
-STEPS = 10_000
+STEPS = 1_000
+TOTAL_EXPERIMENTS = 100
+RW_CLASSES = [
+    CliffStatesTD0ExpectedSarsa,
 
-states = CliffStatesEpsGreedy(H, W)
-states.init_values()
-round_rewards = []
-for step in range(STEPS):
-    states.start_round()
-    while True:
-        try:
-            states.random_walk()
-        except StopIteration as e:
-            break
-        
-    round_rewards.append(states.round_reward)
+    CliffStatesv2,
+    CliffStatesEpsGreedy,
+    CliffStatesOffPolicyPolicyIterationEpsGreedy,
+    CliffStatesTD0Sarsa,
+]
+NAMES = [
+    "On-policy MCES",
+    "On-policy MC $\\varepsilon$-greedy",
+    "Off-policy MC Policy Iteration",
+    "TD(0) Sarsa",
+    "TD(0) Expected Sarsa",
+]
+rr_records = {}
+window_size = STEPS // 20
 
-states.show_policy()
+if __name__ == "__main__":
+    for name, rw_class in zip(NAMES, RW_CLASSES):
+        print(f"Calculating {name}")
+        all_rrs = []
+        # for _ in range(TOTAL_EXPERIMENTS):
+            # states = rw_class(H, W)
+            # states.init_values()
+            # round_rewards = []
+            # for step in range(STEPS):
+            #     rw = states.random_walk()
+            #     round_rewards.append(rw)
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-clrs = sns.color_palette("husl", 2)
-with sns.axes_style("darkgrid"):
-    ax.plot(smoothed(round_rewards, gamma=0.8), c=clrs[0])
+            # states.show_policy()
+            # print("-" * 20)
+            # all_rrs.append(round_rewards)
+        executor = ProcessPoolExecutor(max_workers=16)
+        all_rrs = executor.map(run_exp, [(rw_class, H, W, STEPS) for _ in range(TOTAL_EXPERIMENTS)])
+        all_rrs = list(all_rrs)
+        all_rrs = np.array(all_rrs)
+        # all_rrs = window_average_ndim(all_rrs, window_size=STEPS//100)
+        rr_mean, rr_std = compute_mean_std(all_rrs)
 
-    ax.legend(["On-policy MC"])
+        rr_mean = window_average(rr_mean, window_size=window_size)
+        # rr_mean = smoothed(rr_mean, gamma=0.8)
+        # rr_std = rr_std[:-window_size+1]
+        rr_std = window_average(rr_std, window_size=window_size) / (
+            TOTAL_EXPERIMENTS**0.5
+        )
+        rr_records[name] = (rr_mean, rr_std)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    clrs = sns.color_palette("husl", 5)
+    with sns.axes_style("darkgrid"):
+        for idx, (rr_mean, rr_std) in enumerate(rr_records.values()):
+            ax.plot(rr_mean, c=clrs[idx])
+            ax.fill_between(
+                np.arange(rr_mean.shape[0]),
+                rr_mean + rr_std,
+                rr_mean - rr_std,
+                alpha=0.3,
+                facecolor=clrs[idx],
+            )
+    legends = []
+    for name in rr_records.keys():
+        legends.append(f"{name}, mean over {TOTAL_EXPERIMENTS} times")
+        # legends.append(f"{name}, 0.1 std")
+        legends.append("_nolegend_")
+    ax.legend(legends, loc="lower right")
     ax.set_xlabel("Round")
     ax.set_ylabel("Round Reward")
+    plt.ylim(-40,-20)
 
-fig.savefig("mc.png", dpi=500)
+    fig.savefig("mc.png", dpi=500)
--- mctd/utils.py
+++ mctd/utils.py
@@ -1,41 +1,85 @@
 import numpy as np
 import copy
 import random
+from typing import Tuple
+
 
 def tuple_add(t1, t2):
-    return (t1[0]+t2[0],t1[1]+t2[1])
+    return (t1[0] + t2[0], t1[1] + t2[1])
 
-def smoothed(xs, gamma=0.8):
-    smoothed = xs[:1]
-    for x in xs[1:]:
-        smoothed.append(gamma * x + smoothed[-1] * (1-gamma))
+
+def compute_mean_std(rewards):
+    # rewards: [N, T]
+    mean = np.mean(rewards, axis=0)  # [T]
+    # rewards_t = np.transpose(rewards) # [T, N]
+    std = np.sqrt(np.sum((rewards - mean) ** 2, axis=0) / (rewards.shape[1] - 1))
+    return mean, std
+
+
+def window_average_ndim(xs, window_size=20):
+    window_avg: np.ndarray = np.zeros_like(xs)
+    zeros = np.zeros_like(xs)
+    WINDOW_SIZE = window_size
+    for k in range(1, WINDOW_SIZE + 1):
+        window_avg += np.concatenate((xs[:, k:], zeros[:, :k]), axis=1)
+    for k in range(-WINDOW_SIZE, 0):
+        window_avg += np.concatenate((zeros[:, :-k], xs[:, :k]), axis=1)
+    window_avg += xs
+    window_avg /= (2.0 * WINDOW_SIZE) + 1
+    return window_avg[:, WINDOW_SIZE:-WINDOW_SIZE]
+
+
+def window_average(xs, window_size=20):
+    return np.convolve(xs, np.ones(window_size), "valid") / window_size
+
+
+def smoothed(xs: np.ndarray, gamma=0.8):
+    smoothed = xs.copy()
+    for i, x in zip(range(1, xs.shape[-1]), xs[1:]):
+        smoothed[i] = gamma * x + smoothed[i - 1] * (1 - gamma)
     return smoothed
 
-class CliffStates:
-    SHIFTS = ((-1,0),(1,0),(0,-1),(0,1))
+
+class CliffStatesv2:
+    SHIFTS = ((-1, 0), (1, 0), (0, -1), (0, 1))
+    MAXSTEP = 100
 
     def __init__(self, h, w, gamma=0.9):
         self.h = h
         self.w = w
         self.gamma = 0.9
+        self.terminal = (1, w)
+        self.begin = (1, 1)
         # self.current_position = (1,1)
 
     def init_values(self):
         # initial values
-        self.q_values = {(i,j,a): random.random() for a in range(4) for i in range(1, self.h+1) for j in range(1, self.w+1)}
+        self.q_values = {
+            (i, j, a): random.random()
+            for a in range(4)
+            for i in range(1, self.h + 1)
+            for j in range(1, self.w + 1)
+        }
         # self.avg_return = copy.deepcopy(self.q_values)
-        self.return_counts = {(i,j,a): 0 for a in range(4) for i in range(1, self.h+1) for j in range(1, self.w+1)}
-        self.policy_values = {(i,j): 1/4 for i in range(1,self.h+1) for j in range(1,self.w+1)}
+        self.return_counts = {
+            (i, j, a): 0
+            for a in range(4)
+            for i in range(1, self.h + 1)
+            for j in range(1, self.w + 1)
+        }
+        self.policy_values = {
+            (i, j): 1 / 4 for i in range(1, self.h + 1) for j in range(1, self.w + 1)
+        }
         # edge -> init 1/3
-        for i in range(1,self.h+1):
-            self.policy_values[(i,1)] = 1/3
-            self.policy_values[(i,self.w)] = 1/3
-        for j in range(1,self.w+1):
-            self.policy_values[(1,j)]=1/3
-            self.policy_values[(self.h,j)]=1/3
+        for i in range(1, self.h + 1):
+            self.policy_values[(i, 1)] = 1 / 3
+            self.policy_values[(i, self.w)] = 1 / 3
+        for j in range(1, self.w + 1):
+            self.policy_values[(1, j)] = 1 / 3
+            self.policy_values[(self.h, j)] = 1 / 3
         # corners -> 1/2
-        for corner in [(1,1),(1,self.h),(1,self.w),(self.h,self.w)]:
-            self.policy_values[corner] = 1/2
+        for corner in [(1, 1), (1, self.h), (1, self.w), (self.h, self.w)]:
+            self.policy_values[corner] = 1 / 2
 
     def reward(self, x, y):
         if x == 1:
@@ -46,9 +90,7 @@ class CliffStates:
 
         return -1
 
-    def sample_action(self, ok_mask, i=None, j=None):
-        if i is None:
-            i, j = self.current_position
+    def sample_action(self, ok_mask, i: int, j: int, return_max=False):
         q_values = [self.q_values[(i, j, a)] for a in range(4)]
         maxa, maxval = -1, -1e7
         for a, val in enumerate(q_values):
@@ -57,45 +99,7 @@ class CliffStates:
                 maxval = val
         return maxa
 
-    
-    def show_policy(self):
-        arrows = ["↓","↑","←","→"]
-        for i in range(self.h,0,-1):
-            for j in range(1,self.w+1):
-                ok_mask = [True] * 4
-                if i == 1:
-                    ok_mask[0] = False
-                if i == self.h:
-                    ok_mask[1] = False
-                if j == 1:
-                    ok_mask[2] = False
-                if j == self.w:
-                    ok_mask[3] = False
-                action = self.sample_action(ok_mask, i, j)
-                print(arrows[action],end="")
-            print("")
-
-    def update_policy(self):
-        last_state_action = (*self.last_state, self.last_action)
-        # self.avg_return[last_state_action] += self.round_reward
-        self.return_counts[last_state_action] += 1
-        self.q_values[last_state_action] += (self.round_reward - self.q_values[last_state_action]) / self.return_counts[last_state_action]
-
-    def random_walk(self):
-        x,y = self.current_position
-
-        # update round reward
-        reward = self.reward(x,y)
-        self.round_reward = self.gamma * self.round_reward + reward
-        if self.last_state is not None:
-            if (self.last_state, self.last_action) in self.historical_action_state:
-                raise StopIteration
-            self.historical_action_state.add((self.last_state, self.last_action))
-        
-            # update q values
-            self.update_policy()
-
-        # sample next action
+    def get_ok_mask(self, x, y):
         ok_mask = [True] * 4
         if x == 1:
             ok_mask[0] = False
@@ -105,28 +109,97 @@ class CliffStates:
             ok_mask[2] = False
         if y == self.w:
             ok_mask[3] = False
-        action = self.sample_action(ok_mask)
-        self.last_action = action
-        self.last_state = self.current_position
+        return ok_mask
 
-        self.current_position = tuple_add(self.current_position, self.SHIFTS[action])
+    def show_policy(self):
+        arrows = ["↓", "↑", "←", "→"]
+        for i in range(self.h, 0, -1):
+            for j in range(1, self.w + 1):
+                ok_mask = self.get_ok_mask(i, j)
+                action = self.sample_action(ok_mask, i, j, return_max=True)
+                print(arrows[action], end="")
+            print("")
 
-    def start_round(self):
-        # self.current_position = (random.choice(range(1,self.h+1)), random.choice(range(1,self.w+1)))
-        self.current_position = (1,1)
-        self.last_state, self.last_action = None, None
-        self.round_reward = 0
-        self.historical_action_state = set()
+    def update_policy(self, r: float, x: int, y: int, a: int):
+        last_state_action = (x, y, a)
+        # self.avg_return[last_state_action] += self.round_reward
+        self.return_counts[last_state_action] += 1
+        self.q_values[last_state_action] += (
+            r - self.q_values[last_state_action]
+        ) / self.return_counts[last_state_action]
 
-class CliffStatesEpsGreedy(CliffStates):
+    def sample_route(self, until_end=False, begin=None):
+        historical_action_state = set()
+        if begin is not None:
+            x, y = begin
+        else:
+            x, y = random.choice(range(1, self.h + 1)), random.choice(
+                range(1, self.w + 1)
+            )
+        xs, ys, actions = [x], [y], []
+        # total_steps = 0
+        while True:
+            # total_steps += 1
+            # sample next action
+            ok_mask = self.get_ok_mask(x, y)
+            action = self.sample_action(ok_mask, x, y)
+            action_state = (x, y, action)
+            if until_end:
+                # if total_steps > self.MAXSTEP:
+                #     return [None] * 3
+                if (x, y) == self.terminal:
+                    break
+            else:
+                if action_state in historical_action_state:
+                    break
+
+            historical_action_state.add((x, y, action))
+            x, y = tuple_add((x, y), self.SHIFTS[action])
+            if x == 1 and 1 < y < self.w:
+                x, y = 1, 1
+            actions.append(action)
+            xs.append(x)
+            ys.append(y)
+
+        return xs, ys, actions
+
+    def random_walk(self) -> float:
+        xs, ys, actions = self.sample_route()
+
+        rewards = []
+        reward = 0.0
+        for x, y in zip(xs[::-1], ys[::-1]):
+            reward = self.gamma * reward + self.reward(x, y)
+            rewards.append(reward)
+
+        rewards = rewards[::-1][1:]  # 0-th reward stands for nothing
+
+        for x, y, a, r in zip(xs[:-1], ys[:-1], actions, rewards):
+            self.update_policy(r, x, y, a)
+
+        # return sum(rewards)
+        return reward
+
+    def calculate_reward(self) -> float:
+        xs, ys, _ = self.sample_route(until_end=True, begin=self.begin)
+        if xs is None:
+            return -1e6
+        rewards = []
+        reward = 0.0
+        for x, y in zip(xs[1:], ys[1:]):
+            reward = self.gamma * reward + self.reward(x, y)
+            rewards.append(reward)
+
+        return sum(rewards)
+
+
+class CliffStatesEpsGreedy(CliffStatesv2):
     def __init__(self, h, w, gamma=0.9, eps=0.05):
-        super().__init__(h,w,gamma)
+        super().__init__(h, w, gamma)
         self.eps = eps
-    def sample_action(self, ok_mask, i=None, j=None, return_max=False):
-        # TODO
+
+    def sample_action(self, ok_mask, i, j, return_max=False):
         total_as = sum(ok_mask)
-        if i is None:
-            i, j = self.current_position
         q_values = [self.q_values[(i, j, a)] for a in range(4)]
         maxa, maxval = -1, -1e6
         for a, val in enumerate(q_values):
@@ -135,11 +208,230 @@ class CliffStatesEpsGreedy(CliffStates):
                 maxval = val
         if return_max:
             return maxa
-        
+
         probs = [self.eps / total_as] * 4
         probs[maxa] += 1 - self.eps
         for a, ok in enumerate(ok_mask):
             if not ok:
-                probs[a]=0
-        maxa = random.choices(range(4),weights=probs, k=1)[0]
+                probs[a] = 0
+        maxa = random.choices(range(4), weights=probs, k=1)[0]
         return maxa
+
+
+class CliffStatesOffPolicyPolicyIterationEpsGreedy(CliffStatesv2):
+    def __init__(self, h, w, gamma=0.9, eps=0.05):
+        super().__init__(h, w, gamma)
+        self.eps = eps
+
+    def init_values(self):
+        super().init_values()
+        self.c_values = {
+            (i, j, a): 0.0
+            for a in range(4)
+            for i in range(1, self.h + 1)
+            for j in range(1, self.w + 1)
+        }
+
+    def sample_action(self, ok_mask, i, j, return_max=False):
+        total_as = sum(ok_mask)
+        q_values = [self.q_values[(i, j, a)] for a in range(4)]
+        maxa, maxval = -1, -1e6
+        for a, val in enumerate(q_values):
+            if ok_mask[a] and val > maxval:
+                maxa = a
+                maxval = val
+        if return_max:
+            return maxa
+
+        probs = [self.eps / total_as] * 4
+        probs[maxa] += 1 - self.eps
+        # probs = [1 / total_as] * 4
+        for a, ok in enumerate(ok_mask):
+            if not ok:
+                probs[a] = 0
+        maxa = random.choices(range(4), weights=probs, k=1)[0]
+        p = probs[maxa]
+        return maxa, p
+
+    def sample_route(self, until_end=False, begin=None):
+        historical_action_state = set()
+
+        if begin is not None:
+            x, y = begin
+        else:
+            x, y = random.choice(range(1, self.h + 1)), random.choice(
+                range(1, self.w + 1)
+            )
+        xs, ys, actions, bs = [x], [y], [], []
+        # total_steps = 0
+        while True:
+            # sample next action
+            # total_steps += 1
+            ok_mask = self.get_ok_mask(x, y)
+            action, b = self.sample_action(ok_mask, x, y)
+            action_state = (x, y, action)
+            if until_end:
+                # if total_steps > self.MAXSTEP:
+                #     return [None] * 3
+                if (x, y) == self.terminal:
+                    break
+            else:
+                if action_state in historical_action_state:
+                    break
+            historical_action_state.add((x, y, action))
+            x, y = tuple_add((x, y), self.SHIFTS[action])
+            if x == 1 and 1 < y < self.w:
+                x, y = 1, 1
+            actions.append(action)
+            xs.append(x)
+            ys.append(y)
+            bs.append(b)
+
+        if begin is not None and until_end:
+            return xs, ys, actions
+        
+        return xs, ys, actions, bs
+
+    def update_policy(self, r: float, x: int, y: int, a: int, iw: float):
+        last_state_action = (x, y, a)
+        # self.avg_return[last_state_action] += self.round_reward
+        self.c_values[last_state_action] += iw
+        self.q_values[last_state_action] += (
+            iw
+            * (r - self.q_values[last_state_action])
+            / self.c_values[last_state_action]
+        )
+
+    def random_walk(self) -> float:
+        xs, ys, actions, bs = self.sample_route()
+
+        rewards = []
+        iws = []
+        iw = 1.0
+        reward = 0.0
+        for x, y, b in zip(xs[1::-1], ys[1::-1], bs[::-1]):
+            iws.append(iw)
+            iw /= b
+            reward = self.gamma * reward + self.reward(x, y)
+            rewards.append(reward)
+
+        rewards = rewards[::-1]
+
+        for x, y, a, r, iw in zip(xs[:-1], ys[:-1], actions, rewards, iws):
+            self.update_policy(r, x, y, a, iw)
+            best_action = self.sample_action(
+                self.get_ok_mask(x, y), x, y, return_max=True
+            )
+            if best_action != a:
+                break
+
+        # return sum(rewards)
+        return reward
+
+
+class CliffStatesTD0Sarsa(CliffStatesEpsGreedy):
+    def __init__(self, h, w, gamma=0.9, eps=0.05, alpha=0.1):
+        super().__init__(h, w, gamma, eps)
+        self.alpha = alpha
+        self.terminal = (1, w)
+
+    def init_values(self):
+        super().init_values()
+        for a in range(4):
+            self.q_values[(*self.terminal, a)] = 0
+
+    def update_policy(
+        self, r: float, x: int, y: int, a: int, next_state_action: Tuple[int, int, int]
+    ):
+        self.q_values[(x, y, a)] += self.alpha * (
+            r + self.gamma * self.q_values[next_state_action] - self.q_values[(x, y, a)]
+        )
+
+    def sample_route(self, until_end=True, begin=None, update=False, return_rewards=False):
+        xs, ys, actions, rewards = [], [], [], []
+
+        if begin is not None:
+            x, y = begin
+        else:
+            x, y = random.choice(range(1, self.h + 1)), random.choice(
+                range(1, self.w + 1)
+            )
+        
+        ok_mask = self.get_ok_mask(x, y)
+        action = self.sample_action(ok_mask, x, y)
+        # total_steps = 1
+        while True:
+            reward = self.reward(x, y)
+            xs.append(x)
+            ys.append(y)
+            rewards.append(reward)
+            # if total_steps > self.MAXSTEP:
+            #     return [None] * 3
+            if (x, y) == self.terminal:
+                break
+            # sample next action
+            x_, y_ = tuple_add((x, y), self.SHIFTS[action])
+            if x_ == 1 and 1 < y_ < self.w:
+                x_, y_ = 1, 1
+            ok_mask = self.get_ok_mask(x_, y_)
+            action_ = self.sample_action(ok_mask, x_, y_)
+            if update:
+                self.update_policy(reward, x, y, action, (x_, y_, action_))
+
+            x, y, action = x_, y_, action_
+        
+        if return_rewards:
+            return xs, ys, actions, rewards
+        else:
+            return xs, ys, actions
+    
+
+    def random_walk(self) -> float:
+        _, _, _, rewards = self.sample_route(update=True, return_rewards=True)
+        r = 0
+        for i in range(len(rewards)-1,-1,-1):
+            rewards[i] = self.gamma * rewards[i] + r 
+            r = rewards[i]
+        return rewards[0]
+
+class CliffStatesTD0ExpectedSarsa(CliffStatesTD0Sarsa):
+    def __init__(self, h, w, gamma=0.9, eps_policy=1, alpha=0.1, eps_sampler=0.05):
+        super().__init__(h, w, gamma, eps_sampler, alpha)
+        self.eps_policy = eps_policy
+
+    def update_policy(
+        self, r: float, x: int, y: int, a: int, next_state_action: Tuple[int, int, int]
+    ):
+        x_, y_, a_ = next_state_action
+        ok_mask = self.get_ok_mask(x_, y_)
+        total_as = sum(ok_mask)
+        q_values = np.array([self.q_values[(x_, y_, a)] for a in range(4)])
+
+        maxa = np.argmax(q_values)
+        # assert a == maxa
+
+        probs = np.ones(4) * (self.eps_policy / total_as)
+        probs[maxa] += 1 - self.eps_policy
+        probs[ok_mask] = 0
+        # print(probs)
+        expected_q = float(np.sum(q_values * probs))
+        print(expected_q)
+        # expected_q = self.q_values[next_state_action]
+        self.q_values[(x, y, a)] += self.alpha * (
+            r + self.gamma * expected_q - self.q_values[(x, y, a)]
+        )
+
+def run_exp(args):
+    rw_class, H, W, STEPS = args
+    states = rw_class(H, W)
+    states.init_values()
+    round_rewards = []
+    for step in range(STEPS):
+        rw = states.random_walk()
+        round_rewards.append(rw)
+
+    states.show_policy()
+    print("-" * 20)
+    return round_rewards
+
+
